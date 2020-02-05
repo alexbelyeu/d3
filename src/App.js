57eb0aef311845634f28c608791d11f9f212f450
Alex Belyeu <alex.belyeu@peartherapeutics.com>
2020-02-05 12:52:16 +0100

    Grid Layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import RGL, { WidthProvider } from "react-grid-layout";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const ReactGridLayout = WidthProvider(RGL);
+
+export default class NoDraggingLayout extends React.PureComponent {
+  static defaultProps = {
+    className: "layout",
+    // isDraggable: false,
+    isResizable: false,
+    items: 3,
+    cols: 2,
+    rowHeight: 30,
+    onLayoutChange: function() {}
+  };
+
+  constructor(props) {
+    super(props);
+
+    const layout = this.generateLayout();
+    this.state = { layout };
+  }
+
+  generateDOM() {
+    return Array.from(Array(this.props.items).keys()).map((item, index) => {
+      return (
+        <div key={index}>
+          <span className="text">{index}</span>
+        </div>
+      )
+    })
+  }
+
+  generateLayout() {
+    return Array.from(Array(this.props.items).keys()).map((item, i) => {
+      return {
+        x: (i * 2) % 12,
+        y: Math.floor(i / 6) * 3,
+        w: 2,
+        h: 3,
+        i: i.toString()
+      };
+    });
+  }
+
+  onLayoutChange(layout) {
+    this.props.onLayoutChange(layout);
+  }
+
+  render() {
+    return (
+      <ReactGridLayout
+        layout={this.state.layout}
+        onLayoutChange={this.onLayoutChange}
+        {...this.props}
+      >
+        {this.generateDOM()}
+      </ReactGridLayout>
+    );
+  }
 }
-
-export default App;
